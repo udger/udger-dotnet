ce992af020280619360f06dbc8041e83f4091e8f
Jan Tleskac <milan@DESKTOP-0EV1GNK>
2017-08-14 14:28:55 +0200

    Query list finalized.

--- Udger.Parser.V3/UdgerSqlQuery.cs
+++ Udger.Parser.V3/UdgerSqlQuery.cs
@@ -84,6 +84,212 @@ WHERE
         /// <summary>
         /// Query for operating system columns.
         /// </summary>
-        public const string OsColumns = @"";
+        public const string OsColumns = @"
+            family AS os_family,
+            family_code AS os_family_code,
+            name AS os,
+            name_code AS os_code,
+            homepage AS os_home_page,
+            icon AS os_icon,
+            icon_big AS os_icon_big,
+            vendor AS os_family_vendor,
+            vendor_code AS os_family_vendor_code,
+            vendor_homepage AS os_family_vedor_homepage,
+            'https://udger.com/resources/ua-list/os-detail?os=' || REPLACE(name, ' ', '%20') AS os_info_url
+";
+
+        /// <summary>
+        /// Query of operating system.
+        /// </summary>
+        public static readonly string SqLOs = $@"
+        SELECT
+            ur.rowid,
+            {OsColumns}
+        FROM
+            udger_os_regex ur
+        JOIN
+            udger_os_list ON udger_os_list.id = ur.os_id
+        WHERE
+            ur.rowid=?
+";
+
+        /// <summary>
+        /// Query for client operating system.
+        /// </summary>
+        public static readonly string SqlCLientOs = $@"
+        SELECT
+            {OsColumns}
+        FROM
+            udger_client_os_relation
+        JOIN
+            udger_os_list ON udger_os_list.id = udger_client_os_relation.os_id
+        HERE
+            client_id = ?
+";
+
+        /// <summary>
+        /// Device columns list.
+        /// </summary>
+        public const string DeviceColumns = @"
+            name AS device_class,
+            name_code AS device_class_code,
+            icon AS device_class_icon,
+            icon_big AS device_class_icon_big,
+            'https://udger.com/resources/ua-list/device-detail?device=' || REPLACE(name, ' ', '%20') AS device_class_info_url
+";
+
+        /// <summary>
+        /// Query for device.
+        /// </summary>
+        public static readonly string SqlDevice = $@"
+        SELECT
+            ur.rowid,
+            {DeviceColumns}
+        FROM
+            udger_deviceclass_regex ur
+        JOIN
+            udger_deviceclass_list ON udger_deviceclass_list.id = ur.deviceclass_id
+        WHERE
+            ur.rowid=?
+";
+
+        /// <summary>
+        /// Query for client class.
+        /// </summary>
+        public static readonly string SqlClientClass = $@"
+        SELECT
+            {DeviceColumns}
+        FROM
+            udger_deviceclass_list
+        JOIN
+            udger_client_class ON udger_client_class.deviceclass_id = udger_deviceclass_list.id
+        WHERE
+            udger_client_class.id = ?
+";
+
+        /// <summary>
+        /// IP columns list.
+        /// </summary>
+        public const string IpColumns = @"
+            ip_classification AS ip_classification,
+            ip_classification_code AS ip_classification_code,
+            ip_last_seen AS ip_last_seen,
+            ip_hostname AS ip_hostname,
+            ip_country AS ip_country,
+            ip_country_code AS ip_country_code,
+            ip_city AS ip_city,
+            name AS crawler_name, 
+            ver AS crawler_ver,
+            ver_major AS crawler_ver_major,
+            family AS crawler_family,
+            family_code AS crawler_family_code,
+            family_homepage AS crawler_family_homepage,
+            vendor AS crawler_family_vendor,
+            vendor_code AS crawler_family_vendor_code,
+            vendor_homepage AS crawler_family_vendor_homepage,
+            family_icon AS crawler_family_icon,
+            'https://udger.com/resources/ua-list/bot-detail?bot=' || REPLACE(family, ' ', '%20') || '#id' || udger_crawler_list.id AS crawler_family_info_url,
+            last_seen AS crawler_last_seen,
+            crawler_classification AS crawler_category,
+            crawler_classification_code AS crawler_category_code,
+            respect_robotstxt AS crawler_respect_robotstxt
+";
+
+        /// <summary>
+        /// Query for IP.
+        /// </summary>
+        public static readonly string SqlIp = $@"
+        SELECT
+            {IpColumns}
+        FROM
+            udger_ip_list
+        JOIN
+            udger_ip_class ON udger_ip_class.id=udger_ip_list.class_id
+        LEFT JOIN
+            udger_crawler_list ON udger_crawler_list.id=udger_ip_list.crawler_id
+        LEFT JOIN
+            udger_crawler_class ON udger_crawler_class.id=udger_crawler_list.class_id
+        WHERE
+            ip = ?
+        ORDER BY
+            sequence
+";
+
+        /// <summary>
+        /// Data center columns list.
+        /// </summary>
+        public const string DatacenterColumns = @"
+        name AS datacenter_name,
+        name_code AS datacenter_name_code,
+        homepage AS datacenter_homepage
+";
+
+        /// <summary>
+        /// Query for data center.
+        /// </summary>
+        public static readonly string SqlDatacenter = $@"
+        SELECT
+            {DatacenterColumns}
+        FROM
+            udger_datacenter_range
+        JOIN
+            udger_datacenter_list ON udger_datacenter_range.datacenter_id = udger_datacenter_list.id
+        WHERE
+            iplong_from <= ? AND iplong_to >= ?
+";
+
+        /// <summary>
+        /// Query for data center IPV6 ranges???
+        /// </summary>
+        public static readonly string SqlDatacenterRange6 = $@"
+        SELECT
+            {DatacenterColumns}
+        FROM
+            udger_datacenter_range6
+        JOIN
+            udger_datacenter_list ON udger_datacenter_range6.datacenter_id=udger_datacenter_list.id
+        WHERE
+            iplong_from0 <= ? AND iplong_to0 >= ? AND
+            iplong_from1 <= ? AND iplong_to1 >= ? AND
+            iplong_from2 <= ? AND iplong_to2 >= ? AND
+            iplong_from3 <= ? AND iplong_to3 >= ? AND
+            iplong_from4 <= ? AND iplong_to4 >= ? AND
+            iplong_from5 <= ? AND iplong_to5 >= ? AND
+            iplong_from6 <= ? AND iplong_to6 >= ? AND
+            iplong_from7 <= ? AND iplong_to7 >=?
+";
+
+        /// <summary>
+        /// Query for device regex.
+        /// </summary>
+        public const string SqlDeviceRegex = @"
+        SELECT
+            id,
+            regstring
+        FROM
+            udger_devicename_regex
+        WHERE
+            os_family_code=? AND (os_code='-all-' OR os_code=?)
+        ORDER BY sequence
+";
+
+        /// <summary>
+        /// Query for device name list.
+        /// </summary>
+        public const string SqlDeviceNameList = @"
+        SELECT
+            marketname,
+            brand_code,
+            brand,
+            brand_url,
+            icon,
+            icon_big
+        FROM
+            udger_devicename_list
+        JOIN
+            udger_devicename_brand ON udger_devicename_brand.id=udger_devicename_list.brand_id
+        WHERE
+            regex_id = ? AND code = ?
+";
     }
 }
